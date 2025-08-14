31e2ea109507132ae156a5addfdab0da2b86bdcd
dej <wenjiezzu@163.com>
2025-08-14 10:06:39 +0800

    TestGit

---
+++ springBoot3/springBoot3-012-git/src/main/java/com/powernode/springboot/SpringBoot3012GitApplication.java
@@ -0,0 +1,13 @@
+package com.powernode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3012GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3012GitApplication.class, args);
+    }
+
+}
